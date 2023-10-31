983ce58b5ec75aec86c61cba78300742ae9692da
gitairman <aaronhopkins86@gmail.com>
2023-10-31 13:23:26 -0700

    updated topicsSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 // import reducers
+import { topicsSlice } from '../features/topics/topicsSlice.js';
+import { quizzesSlice } from '../features/quizzes/quizzesSlice.js';
+import { cardsSlice } from '../features/cards/cardsSlice.js';
+
+
 
 export default configureStore({
   reducer: {
-    
+    topics: topicsSlice,
+    quizzes: quizzesSlice,
+    cards: cardsSlice,
   },
 });
---
+++ src/features/topics/topicsSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+
+const initialState = { topics: {} };
+
+export const topicsSlice = createSlice({
+    name: 'topics',
+    initialState: initialState,
+
+    reducers: {
+        addTopic: (state, action) => {
+            const { id, name, icon } = action.payload;
+            state.topics[id] = {
+                id: id,
+                name: name,
+                icon,
+                quizIds: [],
+            };
+        }
+    },
+    extraReducers: {
+        "quizzes/AddQuiz": (state, action) => {
+            const { topicId, id } = action.payload;
+            state.topics[topicId].quizIds.push(id);
+        }
+    }
+});
+
+export { addTopic } = topicsSlice.actions;
+export const selectTopics = (state) => state.topics.topics;
+
+export default topicsSlice.reducer;
